c1e9a56f2fe678484bb206aa00df8f0868fdac1f
HarshasriM <medaharsha17@gmail.com>
2025-09-21 15:51:38 +0530

    added logic for hotel booking

--- stayzy/home/urls.py
+++ stayzy/home/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index,name='index'),
+    path('hotel-details/<slug>/', views.hotel_details, name="hotel_details")
 ]
