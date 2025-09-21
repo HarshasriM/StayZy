61fb6710dffeebd9b743fc37690b5de96592f2d6
HarshasriM <medaharsha17@gmail.com>
2025-09-21 22:45:39 +0530

    moved requirements.txr

--- stayzy/stayzy/settings.py
+++ stayzy/stayzy/settings.py
@@ -74,37 +74,26 @@ WSGI_APPLICATION = "stayzy.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-
-# import dj_database_url
-
-# DEBUG = False
-
-# ALLOWED_HOSTS = ["*"]  # Later you can restrict to your Railway domain
-
-# # Database config
-# DATABASES = {
-#     "default": dj_database_url.config(
-#         default="postgresql://postgres:fcUpsTRrQJIvSkqgbgkPZNyKamFgEWBV@mainline.proxy.rlwy.net:58378/railway"
-#     )
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "stayzy_db",
-        "USER": "postgres",
-        "PASSWORD": "Harsha@123",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }   
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+
+
+
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "stayzy_db",
+#         "USER": "postgres",
+#         "PASSWORD": "Harsha@123",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }   
+# }
 
 
 
